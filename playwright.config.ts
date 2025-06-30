6bbf19e9e83ea18000630861c2cc7a6da0a28a2d
mrjira <misterjittakorn@gmail.com>
2025-06-30 09:54:50 +0700

    new main

---
+++ playwright.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "@playwright/test";
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+const ENV = process.env.ENV || 'dev';
+
+dotenv.config({ path: path.resolve(__dirname, `configs/.env.${ENV}`)});
+const baseURL = process.env.BASE_URL || 'http://localhost:3000';
+console.log(`ENV=${ENV} | BASE_URL=${baseURL}`);
+
+export default defineConfig({
+    testDir: "./tests",
+    use: {
+        headless : true,
+        baseURL,
+        screenshot : "only-on-failure",
+        video: "retain-on-failure",
+    },
+});
